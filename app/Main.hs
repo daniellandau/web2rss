33053a7524d1cfb324b9269680230a947338aeb3
Daniel Landau <daniel@landau.fi>
2015-09-13 11:23:07 +0300

    save the response texts in database

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc >>= putStrLn
+main = migration >> someFunc >>= putStrLn
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,66 @@
--- {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+
+
 module Lib
-    ( someFunc
+    ( someFunc,
+      migration
     ) where
 
 import Network.HTTP.Conduit
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Match
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
 import Text.StringLike
--- import Data.Char
+import Data.Char (chr)
+import Database.Persist.Quasi
+import Database.Persist.MySQL
+import Database.Persist.TH
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Text as T (Text, pack)
+import Control.Monad.IO.Class  (liftIO)
+import Control.Monad.Logger (runStderrLoggingT)
 
-someFunc :: IO String
-someFunc = fetch "https://landau.fi"
-
-
-fetch :: String -> IO String
+fetch :: String -> IO B.ByteString
 fetch url = do
-  foo <- simpleHttp url
-  let tags = parseTags foo
+  response <- simpleHttp url
+  let tags = parseTags (L.toStrict response)
   let body = getBody tags
-  let texts = filter (tagText $ \t -> L.length t > 2) body
-  return $ show texts
-  -- return $ map (chr . fromEnum) (L.unpack foo)
+  let texts = filter (tagText $ \t -> B.length t > 2) body
+  -- return $ show texts
+  -- return $ map ((chr . fromEnum) . (L.unpack)) texts
+  return $ innerText texts
 
 getBody :: StringLike str => [Tag str] -> [Tag str]
-getBody tags = dropWhile (~/= "<body>") tags
+getBody tags = dropWhile (~/= body) tags
+  where body :: String
+        body = "<body>"
+
+share [ mkPersist sqlSettings
+      , mkMigrate "migrateAll"
+      ]
+  $(persistFileWith lowerCaseSettings "models")
+
+save :: Page -> IO ()
+save page = runStderrLoggingT $ withMySQLConn defaultConnectInfo $ \connection ->
+  liftIO $ runSqlConn (insert page) connection >> return ()
+
+
+migration :: IO ()
+migration =
+  runStderrLoggingT $ withMySQLConn defaultConnectInfo $ \connection ->
+  liftIO (runSqlConn (runMigration migrateAll) connection)
+
+
+someFunc :: IO String
+someFunc = do
+  content <- fetch "https://landau.fi"
+  now <- getCurrentTime
+  save (Page (T.pack "https://landau.fi") content now)
+  return . show $ content
