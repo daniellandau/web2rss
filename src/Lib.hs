eb7b2893d39cda2e6d24454863d7e8e784f21640
Daniel Landau <daniel@landau.fi>
2015-09-13 11:54:39 +0300

    can tell whether text has changed since last time

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,13 +23,13 @@ import Database.Persist.Quasi
 import Database.Persist.MySQL
 import Database.Persist.TH
 import Data.Time.Clock (UTCTime, getCurrentTime)
-import Data.Text as T (Text, pack)
+import Data.Text as T (Text, pack, unpack)
 import Control.Monad.IO.Class  (liftIO)
 import Control.Monad.Logger (runStderrLoggingT)
 
-fetch :: String -> IO B.ByteString
+fetch :: Text -> IO B.ByteString
 fetch url = do
-  response <- simpleHttp url
+  response <- simpleHttp $ T.unpack url
   let tags = parseTags (L.toStrict response)
   let body = getBody tags
   let texts = filter (tagText $ \t -> B.length t > 2) body
@@ -47,10 +47,16 @@ share [ mkPersist sqlSettings
       ]
   $(persistFileWith lowerCaseSettings "models")
 
+getLatest :: Text -> IO (Maybe B.ByteString)
+getLatest url = runStderrLoggingT $ withMySQLConn defaultConnectInfo $ \connection ->
+  liftIO $ flip runSqlConn connection $ do
+    pageEntity <- selectFirst [PageUrl ==. url] [Desc PageFetched]
+    return $ fmap (\(Entity _ page) -> pageContent page) pageEntity
+
+
 save :: Page -> IO ()
 save page = runStderrLoggingT $ withMySQLConn defaultConnectInfo $ \connection ->
   liftIO $ runSqlConn (insert page) connection >> return ()
-
 
 migration :: IO ()
 migration =
@@ -60,7 +66,11 @@ migration =
 
 someFunc :: IO String
 someFunc = do
-  content <- fetch "https://landau.fi"
+  let url = "https://landau.fi"
+  content <- fetch url
   now <- getCurrentTime
-  save (Page (T.pack "https://landau.fi") content now)
-  return . show $ content
+  latestSaved <- getLatest url
+  let isSame = maybe False (== content) latestSaved
+  if isSame
+    then return "They are the same"
+    else save (Page url content now) >> return "They differ"
